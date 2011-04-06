c22af1926210f04971f2d3da17b029f47ad6b980
Ryder Ross <ryross@gmail.com>
2011-04-06 08:10:48 -0700

    changing structure to use a different config file reader

--- classes/config/hierarchical.php
+++ classes/config/hierarchical.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Config_File extends Kohana_Config_Reader {
+class Config_Hierarchical extends Config_Reader {
 
 	protected $_env_map = array(
 		1 => 'production',
@@ -9,6 +9,25 @@ class Config_File extends Kohana_Config_Reader {
 		4 => 'development',
 	);
 
+	/**
+	 * @var  string  Configuration group name
+	 */
+	protected $_configuration_group;
+
+	/**
+	 * @var  bool  Has the config group changed?
+	 */
+	protected $_configuration_modified = FALSE;
+
+	public function __construct($directory = 'config')
+	{
+		// Set the configuration directory name
+		$this->_directory = trim($directory, '/');
+
+		// Load the empty array
+		parent::__construct();
+	}
+	
 	public function load($group, array $config = NULL)
 	{
 		if ($files = Kohana::find_file($this->_directory, $group, NULL, TRUE))
@@ -38,8 +57,7 @@ class Config_File extends Kohana_Config_Reader {
 				}
 			}
 		}
-
 		return parent::load($group, $config);
 	}
-
 }
+
--- init.php
+++ init.php
@@ -1,8 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Config extends Kohana_Config {
-
-
-
-}
+Kohana::$config->attach(new Config_Hierarchical);
 
